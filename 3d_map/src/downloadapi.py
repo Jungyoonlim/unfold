86450e475f828a29302c325f0dbbc464398f6bcf
jungyoonlim <jungyoon.lim99@gmail.com>
2023-07-25 15:36:00 -0700

    turbosquid repo used

---
+++ 3d_map/src/downloadapi.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+def authenticate():
+    client_id = os.getenv()
+    client_secret = os.abort
+
+data = {
+    'client_id':
+    'client_secret':
+
+}
+
+
+
+if __name__ == '__main__':
+    main()
