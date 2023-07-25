353efc9f6575a0c2e3447647864ab8e096a0e037
jungyoonlim <jungyoon.lim99@gmail.com>
2023-07-25 15:03:21 -0700

    found turbosquid api. might try that

--- 3d_map/src/download.py
+++ 3d_map/src/download.py
@@ -1,52 +1,80 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import os
 import random
-import os  # Add this import
+import time
 
-def generate_random_ids(n):
-    return [random.randint(100000, 999999) for _ in range(n)]
+def get_free_model_urls():
+    free_model_urls = []
+    base_url = 'https://www.turbosquid.com/Search/3D-Models/free'
+    try:
+        response = requests.get(base_url)
 
-thing_id_list = generate_random_ids(100)
+        if response.status_code != 200:
+            print(f"Failed to get models from URL {base_url}. Server responded with status code {response.status_code}.")
+            return free_model_urls
 
-url = 'https://www.turbosquid.com/Search/3D-Models/free'
-r = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        all_models = soup.find_all('a')  # Get all 'a' tags
 
-def check_uv_mapping(thing_id):
-    url = f"https://www.turbosquid.com/free/3d-models/3d-model-{thing_id}"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+        for model in all_models:
+            free_item = model.find('div', class_='free_item')  # Check if a 'div' with class 'free_item' exists
+            if free_item and free_item.text.strip() == 'Free':  # Check if the text within the div is 'Free'
+                free_model_urls.append(model['href'])
 
-    # Find UV Mapped tag using its ID
-    uv_mapped_tag = soup.find('span', {'id': 'FPSpec_uv_mapped'}) 
+    except Exception as e:
+        print(f"Error occurred while getting free model URLs: {e}")
 
-    if uv_mapped_tag is not None:
-        print(f"UV Mapped tag found for Thing ID: {thing_id}")
-        return True
+    return free_model_urls
 
-    print(f"No UV Mapped tag found for Thing ID: {thing_id}")
-    return False
+def check_uv_mapping(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-def download_thing(thing_id):
-    print(f"Downloading files for thing ID: {thing_id}")
+        # Find UV Mapped tag using its ID
+        uv_mapped_tag = soup.find('span', {'id': 'FPSpec_uv_mapped'})
 
-    url = f"https://www.turbosquid.com/3d-models/3d-model-{thing_id}"
+        if uv_mapped_tag is not None:
+            print(f"UV Mapped tag found for URL: {url}")
+            return True
 
-    raw_directory = '/Users/jungyoonlim/rothko/3d_map/data/raw'
-    os.makedirs(raw_directory, exist_ok=True)  # 'os' is now recognized  
-    response = requests.get(url, stream=True)
+        print(f"No UV Mapped tag found for URL: {url}")
+        return False
+    except Exception as e:
+        print(f"Error occurred while checking UV mapping: {e}")
+        return False
 
-    if response.status_code != 200:
-        print(f"Failed to download thing {thing_id}. Server responded with status code {response.status_code}.")
+def download_thing(url):
+    if not check_uv_mapping(url):
+        print(f"Model at URL {url} is not UV mapped. Skipping download.")
         return
 
-    zip_path = f"{raw_directory}/{thing_id}.zip"
-    with open(zip_path, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
+    print(f"Downloading files for URL: {url}")
 
-    print(f"Successfully downloaded thing {thing_id}")
+    raw_directory = '/Users/jungyoonlim/rothko/3d_map/data/raw'
+    os.makedirs(raw_directory, exist_ok=True)
+    try:
+        response = requests.get(url, stream=True)
 
-for thing_id in thing_id_list:
-    if check_uv_mapping(thing_id):
-        download_thing(thing_id)
+        if response.status_code != 200:
+            print(f"Failed to download from URL {url}. Server responded with status code {response.status_code}.")
+            return
+
+        zip_path = f"{raw_directory}/{os.path.basename(url)}.zip"
+        with open(zip_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+
+        print(f"Successfully downloaded from URL {url}")
+
+    except Exception as e:
+        print(f"Error occurred while downloading: {e}")
+
+free_model_urls = get_free_model_urls()
+print(f"Found {len(free_model_urls)} free models.")
+
+for model_url in free_model_urls:
+    download_thing(model_url)
+    time.sleep(random.uniform(1, 3))
