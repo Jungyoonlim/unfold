e131fde5d69a059b3b4944b36736337c169fb349
jungyoonlim <jungyoon.lim99@gmail.com>
2023-07-29 23:57:19 -0700

    uv_unwrap rl algo, tmr more to dogit add .

--- src/models/UV_unwrap.py
+++ src/models/UV_unwrap.py
@@ -1,77 +1,44 @@
-from gym import spaces
 import gym
-import numpy as np 
-from stable_baselines3 import PPO
+from gym import spaces
+import numpy as np
+import trimesh
 
 class UVunwrap(gym.Env):
     def __init__(self):
         super(UVunwrap, self).__init__()
 
         # Define action and observation space
-        n_actions = 5
+        # The action space will depend on the complexity of your 3D model and where you can cut seams.
+        n_actions = 10
         self.action_space = spaces.Discrete(n_actions)
         
-        # Define observation space
-        obs_height = 64
-        obs_width = 64
-        obs_channels = 1
-        obs_shape = (obs_height, obs_width, obs_channels)
-        self.observation_space = spaces.Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
-
+        # The observation space is the UV map.
+        uv_map_height = 64
+        uv_map_width = 64
+        uv_map_channels = 1  # For simplicity, let's say the UV map is grayscale.
+        uv_map_shape = (uv_map_height, uv_map_width, uv_map_channels)
+        self.observation_space = spaces.Box(low=0, high=255, shape=uv_map_shape, dtype=np.uint8)
+        
+        self.model = trimesh.load_mesh('/Users/jungyoonlim/rothko/data/converted')
 
     def step(self, action):
-        # Apply the action to the environment
-        self.state = apply_action(self.state, action) # placeholder function
+        # Add a seam to the model at the location specified by the action.
+        self.model.add_seam(action)
+        
+        # Unwrap the model to create a new UV map (state).
+        self.state = self.model.unwrap()
+        
+        # Apply a texture and calculate the reward based on how good the texture mapping looks.
+        texture = ...  # Load your texture here.
+        reward = self.model.apply_texture(texture)
 
-        # Calculate the reward based on the rules you've provided
-        reward = 0
-
-        # Reward seams along hard edges
-        if is_seam_along_hard_edge(self.state): # placeholder function
-            reward += 1
-
-        # Reward hidden seams
-        if is_seam_hidden(self.state): # placeholder function
-            reward += 1
-
-        # Penalize noticeable seams
-        if is_seam_noticeable(self.state): # placeholder function
-            reward -= 1
-
-        # Penalize overlapping UVs
-        if is_overlapping(self.state): # placeholder function
-            reward -= 1
-
-        # Check if we're done
-        done = is_done(self.state) # placeholder function
-
-    # Optionally we can pass additional info, we are not using that for now
-    info = {}
-
-    return self.state, reward, done, info
-
+        # Check if the unwrapping is done.
+        done = self.model.is_unwrapped()
+        
+        return self.state, reward, done, {}
 
     def reset(self):
-        self.state = load_initial_state()
+        # Reset the model and the state.
+        self.model.reset()
+        self.state = np.zeros(self.observation_space.shape, dtype=self.observation_space.dtype)
         return self.state
-    
-    def render(self, mode='human'):
-
-    
-
-env = UVunwrap()
-
-model = PPO("MlpPolicy", env, verbose=1)
-
-model.learn(total_timesteps=10000)
-
-model.save("uv_unwrapping_agent")
-
-model = PPO.load("uv_unwrapping_agent")
-
-# test the agent 
-obs = env.reset()
-for i in range(100):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
